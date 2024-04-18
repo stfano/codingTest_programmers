1e7b23fc05ce6acf24d8803dc9ff7b7bb6346ae1
sseungpp <57385696+stfano@users.noreply.github.com>
2024-04-18 17:43:47 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,22 +1,25 @@
-SELECT 
-    RH.HISTORY_ID,
-    FLOOR(
-        CC.DAILY_FEE * (DATEDIFF(RH.END_DATE, RH.START_DATE) + 1) * 
-        (
-            CASE
-                WHEN DATEDIFF(RH.END_DATE, RH.START_DATE) + 1 >= 90 THEN 1 - (SELECT DISCOUNT_RATE FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN WHERE CAR_TYPE = CC.CAR_TYPE AND DURATION_TYPE = '90일 이상') / 100
-                WHEN DATEDIFF(RH.END_DATE, RH.START_DATE) + 1 >= 30 THEN 1 - (SELECT DISCOUNT_RATE FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN WHERE CAR_TYPE = CC.CAR_TYPE AND DURATION_TYPE = '30일 이상') / 100
-                WHEN DATEDIFF(RH.END_DATE, RH.START_DATE) + 1 >= 7 THEN 1 - (SELECT DISCOUNT_RATE FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN WHERE CAR_TYPE = CC.CAR_TYPE AND DURATION_TYPE = '7일 이상') / 100
-                ELSE 1
-            END
-        )
-    ) AS FEE
-FROM 
-    CAR_RENTAL_COMPANY_CAR CC
-JOIN 
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY RH ON CC.CAR_ID = RH.CAR_ID
-WHERE 
-    CC.CAR_TYPE = '트럭'
-ORDER BY 
-    FEE DESC, 
-    RH.HISTORY_ID DESC;
+with rental as (
+select h.CAR_ID, c.CAR_TYPE, c.DAILY_FEE, h.HISTORY_ID,
+        datediff(h.END_DATE, h.START_DATE)+1 as renday
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+left join CAR_RENTAL_COMPANY_CAR c
+    on h.CAR_ID=c.CAR_ID
+    where c.CAR_TYPE in ('트럭')
+),
+discount as (
+select p.DURATION_TYPE, p.DISCOUNT_RATE, c.CAR_TYPE, c.CAR_ID
+from CAR_RENTAL_COMPANY_DISCOUNT_PLAN p
+left join CAR_RENTAL_COMPANY_CAR c
+    on p.CAR_TYPE=c.CAR_TYPE
+    where c.CAR_TYPE in ('트럭')
+)
+
+select r.HISTORY_ID,
+round(r.DAILY_FEE*r.renday*(100-ifnull(d.DISCOUNT_RATE, 0))/100) FEE
+from rental r
+left join discount d
+on r.CAR_ID=d.CAR_ID
+    and case when renday >= 90 then '90일 이상'
+             when renday >= 30 then '30일 이상'
+             when renday >= 7 then '7일 이상' end = d.DURATION_TYPE
+order by 2 desc, 1 desc
