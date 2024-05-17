b0d927a4eba48830dd0954249654fa5d6e861260
sseungpp <57385696+stfano@users.noreply.github.com>
2024-05-17 21:02:21 +0900

    [Gold II] Title: 트리의 지름, Time: 4596 ms, Memory: 78540 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,37 +1,37 @@
 from collections import deque
 
 def bfs(tree, start):
-    queue = deque([(start, 0)])
+    queue = deque([(start,0)])
     visited = set()
-    max_distance = 0
+    max = 0
     max_node = start
-
+    
     while queue:
         node, dis = queue.popleft()
         visited.add(node)
         
-        if dis > max_distance:
-            max_distance = dis
+        if dis > max :
+            max = dis
             max_node = node
         
-        for neighbor, weight in tree[node]:
-            if neighbor not in visited:
-                queue.append((neighbor, dis + weight))
+        for with_node, distance in tree[node]:
+            if with_node not in visited:
+                queue.append((with_node, dis + distance))
                 
-    return max_node, max_distance
+    return max_node, max
 
-V = int(input())
-tree = [[] for _ in range(V + 1)]
+N =  int(input())
 
+tree = [[] for _ in range(N + 1)]
 
-for _ in range(V):
-    info = list(map(int, input().split()))
-    node = info[0]
-    edges = info[1:-1]
-    for i in range(0, len(edges), 2):
-        neighbor, weight = edges[i], edges[i+1]
-        tree[node].append((neighbor, weight))
-
-x, _ = bfs(tree, 1)
-x1, y1 = bfs(tree, x)
-print(y1)
+for _ in range(N):
+    information = list(map(int, input().split()))
+    node = information[0]
+    edges = information[1:-1]
+    for i in range(0,len(edges), 2): # 0, 2, 2
+        with_node, weight = edges[i], edges[i+1]
+        tree[node].append((with_node, weight))
+        
+node1, distance1 = bfs(tree, 1)
+far_node, far_dis = bfs(tree, node1)
+print(far_dis)
