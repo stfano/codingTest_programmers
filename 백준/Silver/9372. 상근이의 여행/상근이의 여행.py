277850928bd45524ce9c7cdbf8e5ac8eef1ab6b5
sseungpp <57385696+stfano@users.noreply.github.com>
2024-05-28 18:30:51 +0900

    [Silver IV] Title: 상근이의 여행, Time: 152 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+graph = {}
+for test in range(T):
+    nation, country = map(int,input().split())
+    for root in range(country):
+        n1, n2 = map(int, input().split())
+    print(nation -1)
